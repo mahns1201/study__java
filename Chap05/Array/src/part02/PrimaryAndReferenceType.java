27fa512df507a70cf38891d4a2afd1bd9f4bf0b8
mahns <mahns1201@gmail.com>
2022-05-03 23:59:42 +0900

    [add] part5

--- Chap05/Array/src/part02/PrimaryAndReferenceType.java
+++ Chap05/Array/src/part02/PrimaryAndReferenceType.java
@@ -3,23 +3,30 @@ package part02;
 import java.util.Arrays;
 
 public class PrimaryAndReferenceType {
+//	public static void main(String[] args) {
+//		// 湲곕낯 �옄猷�
+//		int value1 = 3;
+//		int value2 = value1;
+//		value2 = 7;
+//		
+//		System.out.println(value1); // 3
+//		System.out.println(value2); // 7
+//		
+//		// 李몄“ �옄猷뚰삎
+//		int[] array1 = {1, 2, 3};
+//		int[] array2 = array1;
+//		
+//		array2[0] = 9;
+//		
+//		System.out.println(Arrays.toString(array1)); // [9, 2, 3]
+//		System.out.println(Arrays.toString(array2)); // [9, 2, 3]
+//		
+//	}
 	public static void main(String[] args) {
-		// 기본 자료
-		int value1 = 3;
-		int value2 = value1;
-		value2 = 7;
+		String str1 = new String("hi");
+		System.out.println(str1);
 		
-		System.out.println(value1); // 3
-		System.out.println(value2); // 7
-		
-		// 참조 자료형
-		int[] array1 = {1, 2, 3};
-		int[] array2 = array1;
-		
-		array2[0] = 9;
-		
-		System.out.println(Arrays.toString(array1)); // [9, 2, 3]
-		System.out.println(Arrays.toString(array2)); // [9, 2, 3]
-		
+		String str2 = "hi~!"
+		System.out.println(str2);
 	}
 }
